a6004bbc1a5d6d181ac62bac9772746fb3eb8b6f
Rodion <rosprikut@gmail.com>
2025-10-05 00:48:01 +0300

    Initial comment

---
+++ PiongPiong/PiongPiong/PiongPiongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiongPiongApp.swift
+//  PiongPiong
+//
+//  Created by Rodion on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PiongPiongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
